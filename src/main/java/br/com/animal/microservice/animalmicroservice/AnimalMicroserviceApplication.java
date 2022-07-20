4158bfa5569ae7acc9ad513e123ec22be69753b3
Rodrigo Prado <digosilva.prado@gmail.com>
2022-07-20 20:44:43 -0300

    Criacao projeto maven

---
+++ src/main/java/br/com/animal/microservice/animalmicroservice/AnimalMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.animal.microservice.animalmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/animal/microservice/animalmicroservice/AnimalMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.animal.microservice.animalmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
